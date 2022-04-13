a042b426186cb94e660e76245bbe7fa21d57da68
adamquintal@gmail.com <adamquintal@gmail.com>
2022-04-12 23:11:03 -0400

    club selector function

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
-clubs = ["Driver", "Pitching Wedge", "5 Iron", "7 Iron", "60 Degree"]
-randomClub = Math.floor(Math.random() * 5);
-alert(clubs[randomClub]);
+
+function clubSelector(){
+    let clubs = ["Driver", "Pitching Wedge", "5 Iron", "7 Iron", "60 Degree"]
+    let randomClub = Math.floor(Math.random() * 5);
+    document.getElementById('club').textContent = clubs[randomClub]
+}
